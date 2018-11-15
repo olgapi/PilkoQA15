2cdbe8ebd0cde954dfce4fe00e456843a33d3247
olgapi <olga.pilko@gmail.com>
2018-11-15 18:35:00 +0200

    DAta Provider Groups

--- addressbook-selenium-tests/src/test/java/com/telRan/addressbook/tests/GroupCreationTests.java
+++ addressbook-selenium-tests/src/test/java/com/telRan/addressbook/tests/GroupCreationTests.java
@@ -2,22 +2,53 @@ package com.telRan.addressbook.tests;
 
 import com.telRan.addressbook.model.Group;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-public class GroupCreationTests extends TestBase {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
-  @Test
-  public void testGroupCreation() {
+public class GroupCreationTests extends TestBase {
+  @DataProvider
+  public Iterator<Object[]> validGroups() throws IOException {
+
+    List<Object[]> list = new ArrayList<>();
+
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src\\test\\resources\\groups.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[]{new Group()
+              .setGroupName(split[0])
+              .setGroupHeader(split[1])
+              .setGroupFooter(split[2])});
+      line = reader.readLine();
+    }
+    /*list.add(new Object[]{"test1","header1","footer1"});
+    list.add(new Object[]{"test2","header2","footer2"});
+    list.add(new Object[]{"t","h","f"});
+    list.add(new Object[]{"looooooong","heeeeeeeeeader","foooooooter"});
+    list.add(new Object[]{"","",""});
+*/
+
+
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validGroups")
+  public void testGroupCreation(Group group) {
 
     app.getGroupHelper().openGroupsPage();
     int before = app.getGroupHelper().getGroupsCount();
 
     app.getGroupHelper().initGroupCreation();
 
-    app.getGroupHelper().fillGroupForm(new Group()
-            .setGroupName("Qa15")
-            .setGroupHeader("ghhhh")
-            .setGroupFooter("footer"));
+    app.getGroupHelper().fillGroupForm(group);
+          //  .setGroupName(groupName)
+           // .setGroupHeader(groupHeader)
+           // .setGroupFooter(groupFooter));
 
     app.getGroupHelper().submitGroupCreation();
     app.getGroupHelper().returnToGroupsPage();
